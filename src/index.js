14e0876a29915aea12ea6e869f116a7aa50d9152
SebyGarza <sebastian.garzagarcia@gmail.com>
2025-06-25 19:26:36 -0600

    Firebase changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { firebaseConfig } from './firebase';
-import * as firebase from 'firebase/app';
+import './firebase'; // initializes Firebase (already handles config and app init)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-firebase.initializeApp(firebaseConfig);
 
 root.render(
   <React.StrictMode>
@@ -15,7 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
